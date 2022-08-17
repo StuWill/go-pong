7c6f058451e1a7b05b7f77d68a40f9c12e44d105
StuWill <stuartjjwillcock@gmail.com>
2022-08-17 17:04:46 +0100

    Wip

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"log"
+	"os"
 
 	tcell "github.com/gdamore/tcell/v2"
 )
@@ -15,15 +16,35 @@ func main() {
 	if err := screen.Init(); err != nil {
 		log.Fatalf("%+v", err)
 	}
-	defStyle := tcell.StyleDefault.Background(tcell.ColorBlack).Foreground(tcell.ColorBlack)
+	defStyle := tcell.StyleDefault.Background(tcell.ColorReset).Foreground(tcell.ColorReset)
 	screen.SetStyle(defStyle)
 
+	go Run(screen, defStyle)
+
+	for {
+		switch event := screen.PollEvent().(type) {
+		case *tcell.EventResize:
+			screen.Sync()
+		case *tcell.EventKey:
+			if event.Key() == tcell.KeyEscape || event.Key() == tcell.KeyCtrlC {
+				screen.Fini()
+				os.Exit(0)
+			}
+		}
+	}
+
+}
+
+func Run(screen tcell.Screen, defStyle tcell.Style) {
+	x := 0
 	for {
 
-		screen.SetContent(0, 0, 'H', nil, defStyle)
-		screen.SetContent(1, 0, 'i', nil, defStyle)
-		screen.SetContent(2, 0, '!', nil, defStyle)
+		screen.SetContent(x, 10, 'H', nil, defStyle)
+		screen.SetContent(x+1, 10, 'i', nil, defStyle)
+		screen.SetContent(x+2, 10, '!', nil, defStyle)
 
 		screen.Show()
+
+		x++
 	}
 }
